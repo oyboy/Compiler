eb24b62dec95a9063fb41eae3a1fcfad48b5a973
oyboy <atom_box687@mail.ru>
2026-02-24 22:44:20 +0300

    Lex implementation

---
+++ src/main/java/lexer/Token.java
@@ -0,0 +1,22 @@
+package lexer;
+
+public class Token {
+    public final TokenType type;
+    public final String lexeme;
+    public final Object literal;
+    public final int line;
+    public final int column;
+
+    public Token(TokenType type, String lexeme, Object literal, int line, int column) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d:%d %s %s", line, column, type, lexeme);
+    }
+}
